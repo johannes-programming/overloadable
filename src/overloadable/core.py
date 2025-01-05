a3d56ce80027a7fd11faca6447e35818d277291b
Johannes <johannes-programming@mailfence.com>
2025-01-05 20:17:02 +0100

    Version 1.0.9

--- src/overloadable/core.py
+++ src/overloadable/core.py
@@ -1,66 +1,67 @@
+import dataclasses
 import functools
+import types
 from typing import *
+
+import tofunc
 
 __all__ = ["overloadable"]
 
 
-class Holder: ...
+def identity(value: Any, /) -> Any:
+    return value
 
 
-def identity(old: Any, /) -> Any:
-    return old
+def overloadable(dispatch: Any) -> types.FunctionType:
+    return Data(dispatch).ans
 
 
-def overloadable(
-    old: Callable,
-    /,
-) -> Callable:
-    holder = Holder()
-    try:
-        func = old.__func__
-    except AttributeError:
-        func = old
-        bind = identity
-    else:
-        bind = type(old)
+class Data:
+    def __init__(self, value: Any, /) -> None:
+        self.ans = self.makeans(value)
 
-    @bind
-    @functools.wraps(func)
-    def new(*args, **kwargs) -> Any:
-        key = func(*args, **kwargs)
-        value = holder._data.registry[key]
-        ans = value(*args, **kwargs)
+    def ans_1(self, *args: Any, **kwargs: Any) -> Any:
+        key = self.ans.dispatch(*args, **kwargs)
+        return self.ans.lookup[key](*args, **kwargs)
+
+    def makeans(self, value: Any, /) -> Any:
+        unpack = Unpack.byValue(value)
+        ans = tofunc.tofunc(self.ans_1)
+        functools.wraps(unpack.func)(ans)
+        ans = unpack.kind(ans)
+        ans._data = self
+        ans.lookup = dict()
+        ans.dispatch = unpack.func
+        ans.overload = tofunc.tofunc(self.overload_1)
+        functools.wraps(self.overload_1)(ans.overload)
         return ans
 
-    holder._data = new
-    new.registry = dict()
-    new.overload = functools.partial(
-        overloadtool,
-        bind=bind,
-        data=new,
-    )
-    return new
+    def overload_1(self, key: Any = None) -> Any:
+        return Overload(ans=self.ans, key=key)
 
 
-def overloaddecorator(
-    old: Callable,
-    /,
-    *,
-    bind: Callable,
-    data: Any,
-    key: Hashable,
-) -> Any:
-    data.registry[key] = old
-    overload(bind(old))
-    return data
+@dataclasses.dataclass(frozen=True)
+class Overload:
+    ans: Any
+    key: Any
+
+    def __call__(self, value: Any) -> Any:
+        self.ans.lookup[self.key] = value
+        return self.ans
 
 
-def overloadtool(
-    key: Hashable = None,
-    **kwargs,
-) -> Any:
-    return functools.partial(
-        overloaddecorator,
-        key=key,
-        **kwargs,
-    )
+@dataclasses.dataclass(frozen=True)
+class Unpack:
+    kind: Any
+    func: Any
+
+    @classmethod
+    def byValue(cls, value: Any):
+        try:
+            func = value.__func__
+        except AttributeError:
+            func = value
+            kind = identity
+        else:
+            kind = type(value)
+        return cls(kind=kind, func=func)
