5000a19d2e93b1e5ba7052b9c44e4108edc53666
Johannes <johannes-programming@mailfence.com>
2025-08-27 22:48:03 +0200

    Version 1.0.9+local

--- src/overloadable/core.py
+++ src/overloadable/core.py
@@ -3,40 +3,43 @@ import functools
 import types
 from typing import *
 
-import tofunc
+from identityfunction import identityfunction
+from tofunc import tofunc
 
 __all__ = ["overloadable"]
 
 
-def identity(value: Any, /) -> Any:
-    return value
-
-
 def overloadable(dispatch: Any) -> types.FunctionType:
+    "This function returns an overloadable."
     return Data(dispatch).ans
 
 
 class Data:
-    def __init__(self, value: Any, /) -> None:
+    def __init__(self: Self, value: Any, /) -> None:
+        "This method sets up the current instance."
         self.ans = self.makeans(value)
 
-    def ans_1(self, *args: Any, **kwargs: Any) -> Any:
-        key = self.ans.dispatch(*args, **kwargs)
-        return self.ans.lookup[key](*args, **kwargs)
+    def ans_1(self: Self, *args: Any, **kwargs: Any) -> Any:
+        "This method is used to make the overloadable."
+        key: Any = self.ans.dispatch(*args, **kwargs)
+        ans: Any = self.ans.lookup[key](*args, **kwargs)
+        return ans
 
-    def makeans(self, value: Any, /) -> Any:
-        unpack = Unpack.byValue(value)
-        ans = tofunc.tofunc(self.ans_1)
+    def makeans(self: Self, value: Any, /) -> Any:
+        "This method creates the overloadable."
+        unpack: Any = Unpack.byValue(value)
+        ans: Any = tofunc(self.ans_1)
         functools.wraps(unpack.func)(ans)
         ans = unpack.kind(ans)
         ans._data = self
         ans.lookup = dict()
         ans.dispatch = unpack.func
-        ans.overload = tofunc.tofunc(self.overload_1)
+        ans.overload = tofunc(self.overload_1)
         functools.wraps(self.overload_1)(ans.overload)
         return ans
 
-    def overload_1(self, key: Any = None) -> Any:
+    def overload_1(self: Self, key: Any = None) -> Any:
+        "This method implements the overloading."
         return Overload(ans=self.ans, key=key)
 
 
@@ -45,7 +48,8 @@ class Overload:
     ans: Any
     key: Any
 
-    def __call__(self, value: Any) -> Any:
+    def __call__(self: Self, value: Any) -> Any:
+        "This magic method implements calling the current instance."
         self.ans.lookup[self.key] = value
         return self.ans
 
@@ -56,12 +60,16 @@ class Unpack:
     func: Any
 
     @classmethod
-    def byValue(cls, value: Any):
+    def byValue(cls: type, value: Any) -> Self:
+        "This classmethod creates a new instance from a dispatch."
+        func: types.FunctionType
+        kind: Callable
         try:
             func = value.__func__
         except AttributeError:
             func = value
-            kind = identity
+            kind = identityfunction
         else:
             kind = type(value)
-        return cls(kind=kind, func=func)
+        ans: Self = cls(kind=kind, func=func)
+        return ans
