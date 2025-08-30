3890df0741abc9382fb892c227a48468cc3c353b
Johannes <johannes.programming@gmail.com>
2025-08-30 12:28:04 +0200

    Version 1.0.9+local

--- src/overloadable/core.py
+++ src/overloadable/core.py
@@ -5,63 +5,55 @@ from typing import *
 
 import tofunc
 
-__all__ = ["overloadable"]
+__all__ = ["overloadable", "Overloadable"]
 
 
-def identity(value: Any, /) -> Any:
-    return value
-
-
-def overloadable(dispatch: Any) -> types.FunctionType:
-    return Data(dispatch).ans
-
-
-class Data:
-    def __init__(self, value: Any, /) -> None:
-        self.ans = self.makeans(value)
-
-    def ans_1(self, *args: Any, **kwargs: Any) -> Any:
-        key = self.ans.dispatch(*args, **kwargs)
-        return self.ans.lookup[key](*args, **kwargs)
-
-    def makeans(self, value: Any, /) -> Any:
-        unpack = Unpack.byValue(value)
-        ans = tofunc.tofunc(self.ans_1)
-        functools.wraps(unpack.func)(ans)
-        ans = unpack.kind(ans)
-        ans._data = self
-        ans.lookup = dict()
-        ans.dispatch = unpack.func
-        ans.overload = tofunc.tofunc(self.overload_1)
-        functools.wraps(self.overload_1)(ans.overload)
+def foo(old, *, lookup):
+    def new(*args, **kwargs):
+        key = old(*args, **kwargs)
+        value = lookup[key]
+        ans = value(*args, **kwargs)
         return ans
 
-    def overload_1(self, key: Any = None) -> Any:
-        return Overload(ans=self.ans, key=key)
+    return new
 
 
-@dataclasses.dataclass(frozen=True)
-class Overload:
-    ans: Any
-    key: Any
+class Overloadable:
+    def __init__(self, original):
+        self.original = original
+        self.lookup = dict()
 
-    def __call__(self, value: Any) -> Any:
-        self.ans.lookup[self.key] = value
-        return self.ans
-
-
-@dataclasses.dataclass(frozen=True)
-class Unpack:
-    kind: Any
-    func: Any
-
-    @classmethod
-    def byValue(cls, value: Any):
-        try:
-            func = value.__func__
-        except AttributeError:
-            func = value
-            kind = identity
+    def __get__(self, *args, **kwargs):
+        ans = self.original.__get__(*args, **kwargs)
+        if isinstance(ans, types.FunctionType):
+            ans = foo(ans, lookup=self.lookup)
+            return ans
         else:
-            kind = type(value)
-        return cls(kind=kind, func=func)
+            obj = ans.__self__
+            func = ans.__func__
+            func = foo(func, lookup=self.lookup)
+            ans = types.MethodType(func, obj)
+            return ans
+
+    def __call__(self, *args, **kwargs):
+        # Direct call acts like the plain function
+        key = self.original(*args, **kwargs)
+        value = self.lookup[key]
+        ans = value(*args, **kwargs)
+        return ans
+
+    def overload(self, key=None):
+        return Overload(self, key)
+
+
+class Overload:
+    def __call__(self: Self, value: Any) -> Overloadable:
+        self.overloadable.lookup[self.key] = value
+        return self.overloadable
+
+    def __init__(self: Self, overloadable: Overloadable, key: Any):
+        self.overloadable = overloadable
+        self.key = key
+
+
+overloadable = Overloadable
