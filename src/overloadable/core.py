705e7c61f37a5043f65bec770a14544559f51fea
Johannes <johannes-programming@mailfence.com>
2025-08-30 17:59:31 +0200

    Version 1.0.13

--- src/overloadable/core.py
+++ src/overloadable/core.py
@@ -2,6 +2,8 @@ import functools
 import types
 from typing import *
 
+import datarepr
+
 __all__ = ["overloadable", "Overloadable"]
 
 
@@ -12,14 +14,18 @@ class Overloadable:
     lookup: Any
 
     def __call__(self: Self, *args: Any, **kwargs: Any) -> Any:
+        "This magic method implements self(*args, **kwargs)."
         key: Any = self.dispatch(*args, **kwargs)
         value: Callable = self.lookup[key]
         ans: Any = value(*args, **kwargs)
         return ans
 
     def __get__(
-        self: Self, *args: Any, **kwargs: Any
+        self: Self,
+        *args: Any,
+        **kwargs: Any,
     ) -> types.FunctionType | types.MethodType:
+        "This magic method implements getting as an attribute from a class or an object."
         draft: Any = self.dispatch.__get__(*args, **kwargs)
         ans: Any
         try:
@@ -37,13 +43,23 @@ class Overloadable:
         return ans
 
     def __init__(self: Self, dispatch: Any) -> None:
+        "This magic method sets up self."
         self.dispatch = dispatch
         self.lookup = dict()
+
+    def __repr__(self: Self) -> str:
+        "This magic method implements repr(self)."
+        return datarepr.datarepr(
+            type(self).__name__,
+            dispatch=self.dispatch,
+            lookup=self.lookup,
+        )
 
     def _deco(self: Self, old: Callable) -> Any:
         return deco(old, lookup=dict(self.lookup))
 
-    def overload(self: Self, key=None) -> functools.partial:
+    def overload(self: Self, key: Any = None) -> functools.partial:
+        "This method returns a decorator for overloading."
         return functools.partial(overload_, self, key)
 
 
@@ -52,6 +68,7 @@ overloadable = Overloadable
 
 def deco(old: Callable, *, lookup: dict) -> types.FunctionType:
     def new(*args: Any, **kwargs: Any) -> Any:
+        "This function implements overloaded calling. This docstring should be overwritten."
         key: Any = old(*args, **kwargs)
         value: Any = lookup[key]
         ans: Any = value(*args, **kwargs)
@@ -65,7 +82,13 @@ def deco(old: Callable, *, lookup: dict) -> types.FunctionType:
     return ans
 
 
-def overload_(master: Overloadable, key: Any, value: Callable, /) -> Overloadable:
+def overload_(
+    master: Overloadable,
+    key: Any,
+    value: Callable,
+    /,
+) -> Overloadable:
+    "This function saves a given overload."
     overload(value)
     master.lookup[key] = value
     return master
